503cf8818132322b2a44a854bd73a721a61c9e15
Kamal Nadh Lukka <157940367+kamalnadh219@users.noreply.github.com>
2024-05-30 16:04:31 +0530

    Update SQLQuery1.sql

--- WindowFunctions/SQLQuery1.sql
+++ WindowFunctions/SQLQuery1.sql
@@ -1,4 +1,4 @@
-/*create table employee
+create table employee
 ( id int,
   emp_name varchar(50),
   dept_name varchar(50),
@@ -30,36 +30,43 @@ insert into employee values(121, 'Rosalin', 'IT', 6000);
 insert into employee values(122, 'Ibrahim', 'IT', 8000);
 insert into employee values(123, 'Vikram', 'IT', 8000);
 insert into employee values(124, 'Dheeraj', 'IT', 11000);
-*/
 
---select * from employee;
 
-/*select  dept_name, max(salary) 
+select * from employee;
+
+select  dept_name, max(salary) 
 from employee 
-group by dept_name; */
+group by dept_name; 
 
-/*select *, max(salary) over(partition by dept_name) as highest_Dept_SALARY 
+select *, max(salary) over(partition by dept_name) as highest_Dept_SALARY 
 from employee ;
-*/
 
-/*select * , ROW_NUMBER() over(order by salary  desc ) as top_paid_emp 
+
+select * , ROW_NUMBER() over(order by salary  desc ) as top_paid_emp 
 from employee;
-*/
 
 -- top_paid_emp department wise 
 
---select * , dense_rank() over(partition by dept_name  order by salary  desc) as top_paid_emp 
---from employee;
+select * , dense_rank() over(partition by dept_name  order by salary  desc) as top_paid_emp 
+from employee;
 
-/*select * from 
+select * from 
 (select * , ROW_NUMBER() over(partition by dept_name  order by salary  desc) as top_paid_emp 
 from employee ) as x
 where x.top_paid_emp=1;
-*/
-/*select * , dense_rank() over(partition by dept_name  order by salary  desc) as top_paid_emp 
-from employee;*/
 
+select * , dense_rank() over(partition by dept_name  order by salary  desc) as top_paid_emp 
+from employee;
 
+-- lead and lag
+
+-- fetch a query to display if the salary of an employee is higher, lower or equal to the previous employee.
+select e.*,
+lag(salary) over(partition by dept_name order by emp_id) as prev_empl_sal,
+case when e.salary > lag(salary) over(partition by dept_name order by emp_id) then 'Higher than previous employee'
+     when e.salary < lag(salary) over(partition by dept_name order by emp_id) then 'Lower than previous employee'
+	 when e.salary = lag(salary) over(partition by dept_name order by emp_id) then 'Same than previous employee' end as sal_range
+from employee e;
 
 
 
